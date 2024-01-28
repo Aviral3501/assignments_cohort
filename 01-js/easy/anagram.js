e2fc14f77665fe101d3447e246fc4db09bb11926
Aviral Singh <aviralsingh3501@gmail.com>
2024-01-28 16:32:55 +0530

    Anagram done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
-
-}
-
-module.exports = isAnagram;
+  let length = str1.length;
+  if (length !== str2.length) return false;
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  let j=0;
+  
+  for(let i =0 ;i<length;i++){
+    let char1=str1[i];
+    if(str2.includes(char1)==true){
+      continue;
+    }else{
+      return false;
+    }
+  }
+  return true;
+  }
+  
+  module.exports = isAnagram;
+  
